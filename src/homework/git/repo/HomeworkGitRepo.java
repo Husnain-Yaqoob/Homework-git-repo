73e1c6189c1276d024786b0ca7012d4d6c4f41d9
2022525 <2022525@student.cct.ie>
2023-10-01 16:43:54 +0100

    comments added
    comments has been added as third commit

--- src/homework/git/repo/HomeworkGitRepo.java
+++ src/homework/git/repo/HomeworkGitRepo.java
@@ -18,14 +18,14 @@ public class HomeworkGitRepo {
     public static void main(String[] args) {
        Scanner EO = new Scanner(System.in);
         System.out.println("enter a number");//ask the user to enter a number
-        int num= EO.nextInt();
+        int num= EO.nextInt(); // the num entered to be scanned
         
-       if(isOdd(num)){
-           System.out.println("this is an odd number");
-       } else{
-           System.out.println("this is even number");
+       if(isOdd(num)){// conditionals added to check if number about the even or odd
+           System.out.println("this is an odd number");//output message 
+       } else{ //
+           System.out.println("this is even number");//the alternate thing if the conditins doesnot satisfy
        }
-        EO.close();
+        EO.close();// scanner to be close
     }
     public static boolean isOdd(int num){
         return num%2==1;
